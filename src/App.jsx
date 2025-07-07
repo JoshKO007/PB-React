b3fb43557d0615e6c42d2ffff56c0d31d341c279
Joshtin Yael Cortes López <142925436+JoshKO007@users.noreply.github.com>
2025-07-07 16:33:00 -0600

    Changes

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="app">
+      <h1>¡Hola, Vercel!</h1>
+      <p>Este es un proyecto mínimo de React desplegado en Vercel.</p>
+    </div>
+  )
+}
+
+export default App
