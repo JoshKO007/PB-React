42584e5c4efd5c0de3ff48cc9343415d2023a3f5
JoshKO007 <joshokortes@gmail.com>
2025-07-21 21:30:20 -0600

    fix: wrap routes in Suspense for lazy loading of ConfiguracionUsuario component

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,36 @@
-// src/main.jsx
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Importación normal de componentes que NO fallan
 import App from "./App";
 import InicioSesion from "./pages/inicioSesion";
-import Registro from "./pages/Registro"; 
-import "./index.css";
-import Contacto from "./pages/Contacto"; 
+import Registro from "./pages/Registro";
+import Contacto from "./pages/Contacto";
 import Recuerdo from "./pages/Recuerdo";
-import Galeria from "./pages/Galeria"; 
+import Galeria from "./pages/Galeria";
 import Artista from "./pages/Artista";
-import ConfiguracionUsuario from "./pages/Configuracion";
+
+// ⚠️ Importación diferida del componente problemático
+const ConfiguracionUsuario = React.lazy(() => import("./pages/Configuracion"));
+
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/iniciar-sesion" element={<InicioSesion />} />
-        <Route path="/registro" element={<Registro />} />
-        <Route path="/recuerdo" element={<Recuerdo />} />
-        <Route path="/galeria" element={<Galeria />} />
-        <Route path="/artista" element={<Artista />} />
-        <Route path="/contacto" element={<Contacto />} />
-        <Route path="/configuracion" element={<ConfiguracionUsuario />} />
-      </Routes>
+      <Suspense fallback={<div className="p-8 text-center text-[#a16207]">Cargando configuración...</div>}>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/iniciar-sesion" element={<InicioSesion />} />
+          <Route path="/registro" element={<Registro />} />
+          <Route path="/recuerdo" element={<Recuerdo />} />
+          <Route path="/galeria" element={<Galeria />} />
+          <Route path="/artista" element={<Artista />} />
+          <Route path="/contacto" element={<Contacto />} />
+          <Route path="/configuracion" element={<ConfiguracionUsuario />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   </React.StrictMode>
 );
